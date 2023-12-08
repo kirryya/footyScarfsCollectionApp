11d33c97b0aa222a64428aaaac9a2e401cabe374
kirryya <kir1987@inbox.ru>
2023-12-08 15:15:56 +0300

    edit BrowserRouter

--- src/pages/router-config.tsx
+++ src/pages/router-config.tsx
@@ -1,10 +1,10 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { createHashRouter } from 'react-router-dom';
 
 import { ErrorPage } from '@/common';
 import { routes } from '@/const';
 import { Home, Login, SignUp, Main } from '@/pages';
 
-export const router = createBrowserRouter([
+export const router = createHashRouter([
   { path: '/', element: <Home />, errorElement: <ErrorPage /> },
   { path: routes.LOGIN, element: <Login /> },
   { path: routes.REGISTRATION, element: <SignUp /> },
